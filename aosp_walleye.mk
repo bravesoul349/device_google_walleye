7760b05e1d6c41f9e4ee993048e5769a7a4f6d8c
Lunarixus <Nathan@lunarixus.party>
2020-12-07 22:27:56 +0100

    walleye: Use source built vendor
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 
 $(call inherit-product, device/google/muskie/device-walleye.mk)
-$(call inherit-product-if-exists, vendor/google_devices/muskie/proprietary/device-vendor-walleye.mk)
+$(call inherit-product, vendor/google/walleye/walleye-vendor.mk)
 
 PRODUCT_COPY_FILES += \
     device/google/walleye/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
